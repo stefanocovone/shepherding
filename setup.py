23ff1f157a87c0b8d2f63223ab38bdf684b2d443
stefanocovone <stefanocovone@gmail.com>
2024-05-10 02:58:38 +0200

    updated deps

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="shepherding",
     version="0.0.1",
-    install_requires=["gymnasium>=0.26.1, <1.0.0", "pygame>=2.1.0, <3.0.0"]
+    install_requires=["gymnasium>=0.26.1, <1.0.0", "pygame>=2.1.0, <3.0.0", "numpy"]
 )
